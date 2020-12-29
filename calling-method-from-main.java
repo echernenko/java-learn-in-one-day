cbbf9ee83fd2a191d8868f929790c3828d646f4a
Eugene Chernenko <chernenko@gmail.com>
2020-12-29 14:01:37 +0100

    Create calling-method-from-main.java

---
+++ calling-method-from-main.java
@@ -0,0 +1,11 @@
+class Sandbox {
+
+  public String restoreString() {
+    System.out.println("hallo allemaal");
+    return "merci boku";
+  }
+
+  public static void main(String[] args) {
+    new Sandbox().restoreString();
+  }
+}
